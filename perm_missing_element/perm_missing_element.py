3a5529cacb498b608cff0daa66cf54e32b9ee78d
inessJlassi <inessjlassi2016@gmail.com>
2020-09-30 22:48:25 +0200

    with some changes it works

--- perm_missing_element/perm_missing_element.py
+++ perm_missing_element/perm_missing_element.py
@@ -1,16 +1,9 @@
 
 def solution(A):
-    list = []
-     if A == []:
-        return 2
-    elif len(A) == 1:
-      return 2
-    elif len(A)== 2:
-        return 3
-    else :
-        for i in range(1,len(A)+1):
-            list.append(i)
-        A = sorted(A)
-        for i in range(1,len(A)) :
-            if  A[i]!= list[i]:
-                return list[i]
+    count = 1
+    A = sorted(A)
+    for i in range(0,len(A)):
+        if  A[i] != count :
+            return count
+        count += 1
+    return count
