52e43aad2815e4efcb1f7fc34c0009c6bc4e08bc
inessJlassi <inessjlassi2016@gmail.com>
2020-09-30 15:52:46 +0200

    algorithm with 100% score

--- occurence.py
+++
@@ -1,7 +0,0 @@
-def solution(A):
-    result = 0
-    for number in A:
-      print(result ^= number)
-    return result
-   
-print(solution([9,3,9,3,9,7,9]))
--- odd_occurrences_in_array/occurence.py
+++ odd_occurrences_in_array/occurence.py
@@ -1,7 +1,11 @@
-def occur(arr):
-    
-    for i in arr:
-        if arr.count(i)  == 1 :
-           print(i)
+def occur(arr): 
+    if (len (A))==1 :
+        return A[0]
+    A = sorted(A)
+    for i in range (0,len(A),2):
+        if i+1 == len(A) :
+            return A[i]
+        elif A[i] != A[i+]:
+            return A[i]
     
 print(occur([1,1,2,2,3]))
