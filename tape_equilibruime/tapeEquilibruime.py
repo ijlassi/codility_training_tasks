76dc03d5100226d49cb1e540543c98381f72b9f8
ijlassi <72082217+ijlassi@users.noreply.github.com>
2020-10-01 12:31:31 +0200

    Update tapeEquilibruime.py

--- tape_equilibruime/tapeEquilibruime.py
+++ tape_equilibruime/tapeEquilibruime.py
@@ -1,22 +1,18 @@
-import math
-def diff(arr):
-    s = 0
-    list = []
-    l = []
-    k = []
-    f = 0
-    for i in range(0,len(arr)-1) :
-        s = s + arr[i]
-        list.append(s)
-    res = arr[::-1]
+def solution(A):
+    ans_1= 0
+    ans_2 =0
+    list_1 = []
+    list_2 = []
+    list_3 = []
+    for i in range(0,len(A)-1) :
+        ans_1 = ans_1 + A[i]
+        list_1.append(ans_1)
+    res = A[::-1]
     for i in range(0,len(res)-1):
-        f = f + res[i]
-        l.append(f)
-    r = l[::-1]
-    for i,v in zip(list,r):
-        n = abs(i-v)
-        k.append(n)
-    return  min(k)
-
-
-print(diff([3,1,2,4,3]))
+        ans_2 = ans_2 + res[i]
+        list_2.append(ans_2)
+    list_4 = list_2[::-1]
+    for i,v in zip(list_1,list_4):
+        val = abs(i-v)
+        list_3.append(val)
+    return  min(list_3)
