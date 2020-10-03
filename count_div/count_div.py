5d6dd55e0a9ffb84f7f8cfc6b6ca5b95617cd388
inessJlassi <inessjlassi2016@gmail.com>
2020-10-03 23:22:19 +0200

    Some changes

--- count_div/count_div.py
+++ count_div/count_div.py
@@ -1,8 +1,8 @@
 def solution(A,B,K):
-     count =0
-     for i in range(A,B):
-          if count % K == 0:
-                count = count +1
-        return count
+     if A % K != 0:
+          return 0
+     else:
+          return (B//K)-(A//K)+1
+
 
 
