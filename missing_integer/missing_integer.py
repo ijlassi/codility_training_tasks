0f593d28693c164b735f1ec1ed9d119a97c23546
inessJlassi <inessjlassi2016@gmail.com>
2020-10-03 11:59:33 +0200

    with other some chnages worked with 100% score

--- missing_integer/missing_integer.py
+++ missing_integer/missing_integer.py
@@ -3,20 +3,13 @@ def solution (A):
     min = 1
     for i,v in enumerate(A,1) :
         lista.add(v)
-        if v < 0:
-            return 1
-    s = list(lista)
-    s = sorted(s)
-    for i in range(1,len(s)+1):
-        if len(s)== len(A):
-            return len(s)+1 
-        elif len(s) < len(A):
-            for i,v in enumerate(s,1):
-                if i == v:
-                    min += 1
-            return min
-        else :
-            return 1
+    s = sorted(list(lista))
+    for i in s:
+        if i == min:
+            min += 1
+    return min
+    
+
         
 
 print(solution([1, 3, 6, 4, 1, 2]))
