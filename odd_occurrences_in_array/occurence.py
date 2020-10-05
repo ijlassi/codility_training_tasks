a0a72d05f8a3fb86275fa58e433b6e470b94385f
inessJlassi <inessjlassi2016@gmail.com>
2020-10-05 13:06:51 +0200

    100% score

--- odd_occurrences_in_array/occurence.py
+++ odd_occurrences_in_array/occurence.py
@@ -5,7 +5,7 @@ def solution(A):
     for i in range (0,len(A),2):
         if i+1 == len(A) :
             return A[i]
-        elif A[i] != A[i+]:
+        elif A[i] != A[i+1]:
             return A[i]
     
 
