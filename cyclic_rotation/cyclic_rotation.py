092ed369b7558d5b6ed81059d51d21253d1bb37f
inessJlassi <inessjlassi2016@gmail.com>
2020-10-05 12:36:11 +0200

    algorithm work perfectly

--- cyclic_rotation/cyclic_rotation.py
+++ cyclic_rotation/cyclic_rotation.py
@@ -2,7 +2,7 @@ def arrRotate(A):
     list = A.copy()
     for i in range(1,len(arr)):
         A[0] = list[len(arr)-1]
-        Ai]= list[i-1]
+        A[i]= list[i-1]
     
 def solution(A,K):
     for i in range(K):
