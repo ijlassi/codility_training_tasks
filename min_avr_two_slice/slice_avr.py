906f8785219ef26efb94d466f2c62872b82eba1e
inessJlassi <inessjlassi2016@gmail.com>
2020-10-06 12:51:18 +0200

    algorithm work with 100% correctness and just 20% performance

--- min_avr_two_slice/slice_avr.py
+++ min_avr_two_slice/slice_avr.py
@@ -6,19 +6,11 @@ def solution(A):
     result = 0
     for  i   in range(0,N -1):
         sum = A[i]
-        print("A[i] = ", A[i])
         for  j in  range(i+1, N) :
             if j < i+3:
                 sum += A[j]
-                print("A[j] = :", A[j])
-                print("sum = :", sum)
                 avg = (sum / (j-i+1))
-                print(avg)
                 if (avg < minAvg) :
                      minAvg = avg
                      result = i
-    return result  
-        
-           
-
-print(solution([4,2,2,5,1,5,8]))
+    return result
