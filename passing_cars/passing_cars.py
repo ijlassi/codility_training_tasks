b1562121f89c0217cbfdce817a4e183b40973bb4
inessJlassi <inessjlassi2016@gmail.com>
2020-10-06 14:29:36 +0200

    algorithm works with 100% score

--- passing_cars/passing_cars.py
+++ passing_cars/passing_cars.py
@@ -1,8 +1,13 @@
 def solution(A):
-    count = 0
+    count_0 = 0
+    count_1 = 0
     for P in range(0,len(A)):
         if A[P]== 0:
-            for Q in range(0,len(A)):
-                if P < Q and A[P] != A[Q] :
-                    count += 1
-    return count 
+            count_0 += 1
+        else:
+            count_1 += count_0
+        if  count_1 > 1000000000:
+           return -1
+    print(count_1) 
+
+    print(solution([0,1,0,1,1]))
