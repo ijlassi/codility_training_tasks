1228c8ca974de05b86ee50f69d87c0a4edb00456
inessJlassi <inessjlassi2016@gmail.com>
2020-10-09 18:10:46 +0200

    Algorithm 100% correct and 100% performance

--- max_product_triplet/max_product_triplet.py
+++ max_product_triplet/max_product_triplet.py
@@ -1,15 +1,12 @@
 def solution(A):
-   list = []
-    pr = 0
-    ans =0
-    for P in range(0,len(A)):
-        for Q in range(0,len(A)):
-            if P < Q :
-                for R in range(len(A)):
-                    if Q < R :
-                        pr = A[P]*A[Q]*A[R]
-                        list.append(pr)
-                        
-    return max(list)
+   A =sorted(A)
+   for i in range (0,len(A)):
+       if A[0]*A[1]<0 :
+           return A[len(A)-1]*A[len(A)-2]*A[len(A)-3]
+       if A[0]*A[1]>= 0 :
+           if  A[0]*A[1]*A[len(A)-1]> A[len(A)-1]*A[len(A)-2]*A[len(A)-3]:
+               return A[0]*A[1]*A[len(A)-1]
+           else :
+               return A[len(A)-1]*A[len(A)-2]*A[len(A)-3]
 
     
