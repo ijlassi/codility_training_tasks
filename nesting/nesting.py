1681b3cb5cafd9e49275368799025b9bc15edcfe
inessJlassi <inessjlassi2016@gmail.com>
2020-10-11 19:26:23 +0200

    Algorithm work with 100% score

---
+++ nesting/nesting.py
@@ -0,0 +1,20 @@
+def solution(S):
+    opening = ["("]
+    closing = [")"]
+    list_ = []
+
+    if len(S) == 0:
+        return 1
+    for s in S:
+        if s in opening:
+            list_.append(s)
+        elif s in closing:
+            if len(list_) < 1:
+                return 0
+            element = list_.pop()
+            if opening.index(element) != closing.index(s):
+                return 0
+    if len(list_) == 0:
+        return 1
+    return 0
+  
