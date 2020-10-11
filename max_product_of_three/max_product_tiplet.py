a40d6d5bf0fb35c4c75bd012fac2af7101217231
inessJlassi <inessjlassi2016@gmail.com>
2020-10-11 16:25:13 +0200

    Some changes

--- max_product_of_three/max_product_tiplet.py
+++ max_product_of_three/max_product_tiplet.py
@@ -1,7 +1,7 @@
 def solution(A):
-   A =sorted(A)
+   A = sorted(A)
    for i in range (0,len(A)):
-       if A[0]*A[1]<0 :
+       if A[0] * A[1] < 0 :
            return A[len(A)-1]*A[len(A)-2]*A[len(A)-3]
        if A[0]*A[1]>= 0 :
            if  A[0]*A[1]*A[len(A)-1]> A[len(A)-1]*A[len(A)-2]*A[len(A)-3]:
