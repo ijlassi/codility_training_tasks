d2b4ed3c91c57e77091fab1923f45d680e2dd7d3
inessJlassi <inessjlassi2016@gmail.com>
2020-10-12 13:42:30 +0200

    Algorithm work wtih 100% score

---
+++ stone_wall/stone_wall.py
@@ -0,0 +1,22 @@
+def solution(H):
+    n = len(H)
+    count = 1
+    stack = [H[0]]
+    for i in range(1, n):
+        if H[i] == stack[-1]:
+            continue
+        elif H[i] < stack[-1]:
+          while H[i] < stack[-1]:
+              stack.pop()
+              if (len(stack) == 0) or (H[i] > stack[-1]):
+                  stack.append(H[i])
+                  count += 1
+                  break
+              elif stack[-1] == H[i]:
+                  break
+        else:
+            stack.append(H[i])
+            count += 1
+                   
+    return count
+
