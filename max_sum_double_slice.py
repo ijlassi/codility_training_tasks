11127e1d4b7ecde5ed84aad85e975082a6fdbb99
inessJlassi <inessjlassi2016@gmail.com>
2020-10-13 20:24:33 +0200

    Algorithm with 100% score

---
+++ max_sum_double_slice.py
@@ -0,0 +1,13 @@
+def solution(A): 
+    n = len(A)
+    max_double_slice_sum = 0
+    current_double_slice_sum = 0
+    current_left_slice_sum = 0
+    for z in range(3, n):
+        current_left_slice_sum = max(0, current_left_slice_sum + A[z - 2])
+        print(current_left_slice_sum)
+        current_double_slice_sum = max(current_double_slice_sum + A[z - 1],current_left_slice_sum)
+        print(current_double_slice_sum)
+        max_double_slice_sum = max(max_double_slice_sum,current_double_slice_sum)
+        print(max_double_slice_sum)
+    return max_double_slice_sum
