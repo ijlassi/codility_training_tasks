fcbb5601604ef65685154def244109882298f217
inessJlassi <inessjlassi2016@gmail.com>
2020-10-13 20:23:55 +0200

    Algorithm with 100% score

--- max_sum_double_slice/max_sum_double_slice.py
+++ max_sum_double_slice/max_sum_double_slice.py
@@ -1,26 +1,11 @@
 def solution(A): 
-    maxi = 0
-    answer_1 = 0
-    answer_2 = 0
-    answer = 0 
-    j = 0
-    k = 0 
-    for i in range(len(A)):
-        for j in range(i+1,len(A)-1):
-            for k in range(j+1,len(A)):
-                if j - i == 1 or k - j == 1:
-                     answer = answer + 0
-                if j - i >= 2 :
-                    for v in range(i+1, j):
-                        answer_1 = answer_1 + A[v]
-                if k - j >= 2:
-                    for w in range(j+1, k):
-                        answer_2 = answer_2 + A[w]
-                answer = answer_1 + answer_2
-                answer_1 = 0
-                answer_2 = 0
+    n = len(A)
+    max_double_slice_sum = 0
+    current_double_slice_sum = 0
+    current_left_slice_sum = 0
+    for z in range(3, n):
+        current_left_slice_sum = max(0, current_left_slice_sum + A[z - 2])
+        current_double_slice_sum = max(current_double_slice_sum + A[z - 1],current_left_slice_sum)
+        max_double_slice_sum = max(max_double_slice_sum,current_double_slice_sum)
+    return max_double_slice_sum
 
-                if answer > maxi :
-                    maxi = answer
-                    answer = 0
-    return maxi
