1ae2efc5b3ccc3e09bc4efc56676d167d7b545cc
inessJlassi <inessjlassi2016@gmail.com>
2020-10-19 22:52:03 +0200

    Algorithm 100% correct with 50% performance

--- choclate_by_number/choclate_by_number.py
+++ choclate_by_number/choclate_by_number.py
@@ -1,19 +1,12 @@
 def solution(N, M):
-    choclate_eated = [0]
-    count_choclate = 0
-    results = 0
-    for i in range(0,N):
-        count_choclate = count_choclate + M 
-        if count_choclate < N :
-            choclate_eated.append(count_choclate)
-        if count_choclate > N:
-            results = count_choclate - N 
-            while results >= N :
-                results = results - N
-            if results not in choclate_eated:
-                choclate_eated.append(results)
-            if results < N and results not in choclate_eated :
-                choclate_eated.append(results)
-    return len(choclate_eated)
-
+    X = 0
+    count = 1
+    result = [0]
+    while True:
+        X = (X+M) % N
+        if X == 0:  
+            break  
+        else:
+            count += 1        
+    return count
 
