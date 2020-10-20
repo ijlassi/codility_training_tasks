390cdd74f795edce4ea55a8741b9c90a7c0d8b0d
inessJlassi <inessjlassi2016@gmail.com>
2020-10-20 16:33:54 +0200

    Algorithm with 100% score

--- common_prime_divisors/prime_divisors.py
+++ common_prime_divisors/prime_divisors.py
@@ -1,12 +1,26 @@
+def gcd(a,b):
+    if b == 0:
+       return a 
+    return gcd(b,a % b)
+    
 def solution(A,B):
     count = 0 
-    for i in range(0,len(A)):)
-       mod = max(A[i],B[i]) % min(A[i],B[i]) 
-       if mod == 0:
-           while ( math.gcd(A[i], p) != 1):
-                A[i]= A[i]// math.gcd(A[i], p)
-           while (math.gcd(B[i], p) != 1):
-                B[i] = B[i] // math.gcd(B[i], p)
-           if ( A[i] == 1 and B[i] == 1) :
-                count = count + 1
+    for i in range(0,len(A)):
+       a = A[i]
+       b = B[i]
+       p = gcd(a,b) 
+       while True :
+          d = gcd(a,p)
+          if d == 1:
+             break
+          a = a // d 
+       while True :
+          d = gcd(b,p)
+          if d == 1:
+             break
+          b = b // d 
+       if a == 1 and b == 1 :
+         count = count + 1
+       else :
+           0
     return count 
