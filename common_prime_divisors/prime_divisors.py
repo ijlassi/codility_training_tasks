e1d73b9051d432ac27f16b82663c1dd7e567ae24
inessJlassi <inessjlassi2016@gmail.com>
2020-10-20 16:18:26 +0200

    Algorithm with 81% correctness and 50% performance

--- common_prime_divisors/prime_divisors.py
+++ common_prime_divisors/prime_divisors.py
@@ -1,9 +1,12 @@
 def solution(A,B):
     count = 0 
-    for i in range(0,len(A)):
-       div = max(A[i],B[i]) // min(A[i],B[i])
+    for i in range(0,len(A)):)
        mod = max(A[i],B[i]) % min(A[i],B[i]) 
        if mod == 0:
-           if min(A[i],B[i]) % div == 0:
-               count = count + 1
+           while ( math.gcd(A[i], p) != 1):
+                A[i]= A[i]// math.gcd(A[i], p)
+           while (math.gcd(B[i], p) != 1):
+                B[i] = B[i] // math.gcd(B[i], p)
+           if ( A[i] == 1 and B[i] == 1) :
+                count = count + 1
     return count 
