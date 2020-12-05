aabdf97b905c000c23ac005a87593fc466e616f8
inessJlassi <inessjlassi2016@gmail.com>
2020-12-05 23:35:57 +0100

    Algorithm 100% correct and performant

--- tape_equilibruime/tapeEquilibruime.py
+++ tape_equilibruime/tapeEquilibruime.py
@@ -1,18 +1,18 @@
 def solution(A):
-    ans_1= 0
-    ans_2 =0
-    list_1 = []
-    list_2 = []
-    list_3 = []
+    answer1= 0
+    answer2= 0
+    list1= []
+    list2= []
+    list3= []
     for i in range(0,len(A)-1) :
-        ans_1 = ans_1 + A[i]
-        list_1.append(ans_1)
+        answer1 = answer1 + A[i]
+        list_1.append(answer1)
     res = A[::-1]
     for i in range(0,len(res)-1):
-        ans_2 = ans_2 + res[i]
-        list_2.append(ans_2)
-    list_4 = list_2[::-1]
-    for i,v in zip(list_1,list_4):
+        answer2 = answer2 + res[i]
+        list2.append(answer2)
+    list4 = list2[::-1]
+    for i,v in zip(list1,list4):
         val = abs(i-v)
-        list_3.append(val)
-    return  min(list_3)
+        list3.append(val)
+    return  min(list3)
