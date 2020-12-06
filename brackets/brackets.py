667a431a8b20cec23f7a287baee498701b2d40e2
inessJlassi <inessjlassi2016@gmail.com>
2020-12-06 15:28:24 +0100

    Algorithm 100% correct and 100% performant

--- brackets/brackets.py
+++ brackets/brackets.py
@@ -4,14 +4,14 @@ def solution(S):
     list1 = []
     if len(S) == 0:
         return 1
-    for s in S:
-        if s in opening:
-            list1.append(s)
-        elif s in closing:
+    for item in S:
+        if item in opening:
+            list1.append(item)
+        elif item in closing:
             if len(list1) < 1:
                 return 0
             element = list1.pop()
-            if opening.index(element) != closing.index(s):
+            if opening.index(element) != closing.index(item):
                 return 0
     if len(list1) == 0:
         return 1
