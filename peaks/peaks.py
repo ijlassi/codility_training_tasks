b102c4a9d238a2aacc4dd2f3e3974154dda3b4b0
inessJlassi <inessjlassi2016@gmail.com>
2020-12-06 23:38:48 +0100

    Algorithm 100% correct and 40% performant

--- peaks/peaks.py
+++ peaks/peaks.py
@@ -3,10 +3,10 @@ def solution(A):
     found_cnt = 0
     if len(A) < 3 :
         return 0
-    for v in range(1,len(A)-1):
+    for v in range(1, len(A)-1):
        if A[v] > A[v + 1] and A[v] > A[v - 1]:
            list_indexes.append(v)
-    for i in range(len(list_indexes),0,-1):
+    for i in range(len(list_indexes), 0, -1):
         res = len(A)% i
         if res == 0 :
             block_size = len(A) // i
