569531efa2f172f8abe847e3655a351ca6437113
inessJlassi <inessjlassi2016@gmail.com>
2020-12-06 18:24:05 +0100

    Algorithm 100% correct and 100% performant

--- max_profit/max_profit.py
+++ max_profit/max_profit.py
@@ -3,8 +3,8 @@ def solution(A):
     maxi = 0
     if len(A) < 2 :
         return 0
-    max_profit = A[len(A)-1]
-    for i in range(len(A)-2, -1, -1):
-        maxi = max(maxi, max_profit-A[i])
+    max_profit = A[len(A) - 1]
+    for i in range(len(A) - 2, -1, -1):
+        maxi = max(maxi, max_profit - A[i])
         max_profit = max(A[i], max_profit)
     return maxi
