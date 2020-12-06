9cfb663d84fdc00d71dbc61ac4562d6afc2ccf2f
inessJlassi <inessjlassi2016@gmail.com>
2020-12-06 15:14:33 +0100

    Algorithm 100% correct and 100% performant

--- binary_gap/binary_gap.py
+++ binary_gap/binary_gap.py
@@ -1,22 +1,22 @@
 def solution(N): 
     if (N == 0 or (N & (N - 1)) == 0): 
         return 0
-    bit= 1
-    prev= 0
+    bit = 1
+    prev = 0
     i= 1
     while(i < 33): 
         prev += 1
         if ((N & bit) == bit): 
-            bit= bit << 1
+            bit = bit << 1
             break
-        bit= bit << 1
-    max= -10**9
-    cur= prev 
+        bit = bit << 1
+    max = -10**9
+    cur = prev 
     for j in range(i + 1, 33): 
         cur += 1
         if ((N & bit) == bit): 
             if (max < (cur - prev - 1)): 
-                max= cur - prev - 1
+                max = cur - prev - 1
             prev = cur 
-        bit= bit << 1
+        bit = bit << 1
     return max 
