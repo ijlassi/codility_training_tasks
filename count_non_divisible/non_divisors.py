7bb72eb8b4a7db563fbffd67a8b01ce2e2d2da77
inessJlassi <inessjlassi2016@gmail.com>
2020-12-06 21:58:55 +0100

    Algorithm 80% correct and 0% performant

--- count_non_divisible/non_divisors.py
+++ count_non_divisible/non_divisors.py
@@ -3,7 +3,7 @@ def solution(A):
     list_non_divisors = []
     if len(A) <= 1:
         return 0 
-    for i in range(0,len(A)):
+    for i in range(0, len(A)):
        for j in range(0, len(A)):
            if A[i]% A[j] != 0 :
                count = count +1 
