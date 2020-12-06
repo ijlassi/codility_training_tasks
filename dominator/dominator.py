f66484df52c6cf9ac2ded6f0b024f6e4ab66d115
inessJlassi <inessjlassi2016@gmail.com>
2020-12-06 23:06:50 +0100

    Algorithm 100% correct and 80% performant

--- dominator/dominator.py
+++ dominator/dominator.py
@@ -3,12 +3,12 @@ def solution(A):
    number = 0
    res = 0
    half = len(A)//2 
-   if len(A)== 0:
+   if len(A) == 0:
        return -1
    for i in A :
        res = A.count(i)
        if res > count :
-           count =  res
+           count = res
            number = A.index(i)
            if count > len(A)//2 :
                return number
