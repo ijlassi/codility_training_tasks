ce61590ca7fdddfe65a20e1249d011da93cc62d5
inessJlassi <inessjlassi2016@gmail.com>
2020-12-06 23:40:20 +0100

    Algorithm 100% correct and 100% performant

--- perm_missing_element/perm_missing_element.py
+++ perm_missing_element/perm_missing_element.py
@@ -1,7 +1,7 @@
 def solution(A):
     count = 1
     A = sorted(A)
-    for i in range(0,len(A)):
+    for i in range(0, len(A)):
         if  A[i] != count :
             return count
         count += 1
