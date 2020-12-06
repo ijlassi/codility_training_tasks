df9996d50fa283fdb404925436a303df4769fe66
inessJlassi <inessjlassi2016@gmail.com>
2020-12-06 18:36:44 +0100

    Algorithm 100% correct and 100% performant

--- odd_occurrences_in_array/occurence.py
+++ odd_occurrences_in_array/occurence.py
@@ -1,11 +1,11 @@
 def solution(A): 
-    if len (A) == 1 :
+    if len (A) == 1:
         return A[0]
     A = sorted(A)
-    for i in range (0,len(A),2):
-        if i+1 == len(A) :
+    for i in range (0, len(A), 2):
+        if i + 1 == len(A):
             return A[i]
-        elif A[i] != A[i+1]:
+        elif A[i] != A[i + 1]:
             return A[i]
     
 
