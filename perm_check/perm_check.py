81020a7b59bc15f8686a42fcd60f9fe8c2d3eacf
inessJlassi <inessjlassi2016@gmail.com>
2020-12-06 18:42:08 +0100

    Algorithm 100% correct and 100% performant

--- perm_check/perm_check.py
+++ perm_check/perm_check.py
@@ -1,12 +1,12 @@
 def solution(A):
     count = 0
-    lista = []
+    list1 = []
     for i in range(1,len(A)+1):
-        lista.append(i)
+        list1.append(i)
     A= sorted(A)
-    for i in range(len(lista)) :
-        if  A[i]== list[i]:
-            count = count +1
+    for i in range(len(list1)) :
+        if  A[i] == list1[i]:
+            count = count + 1
     if count == len(A):
         return 1
     return 0
