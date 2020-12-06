41eb06e6534b8d78f29059163dd58264a73de57f
inessJlassi <inessjlassi2016@gmail.com>
2020-12-06 14:59:03 +0100

    Algorithm 100% correct and 100% performant

--- max_sum_double_slice/max_sum_double_slice.py
+++ max_sum_double_slice/max_sum_double_slice.py
@@ -1,9 +1,8 @@
 def solution(A): 
-    n = len(A)
     max_double_slice_sum = 0
     current_double_slice_sum = 0
     current_left_slice_sum = 0
-    for z in range(3, n):
+    for z in range(3, len(A)):
         current_left_slice_sum = max(0, current_left_slice_sum + A[z - 2])
         current_double_slice_sum = max(current_double_slice_sum + A[z - 1],current_left_slice_sum)
         max_double_slice_sum = max(max_double_slice_sum,current_double_slice_sum)
