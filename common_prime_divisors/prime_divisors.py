5b0880725b591b647d62b41e6eb9e3456e22f599
inessJlassi <inessjlassi2016@gmail.com>
2020-12-06 17:00:42 +0100

    Algorithm 100% correct and 100% performant

--- common_prime_divisors/prime_divisors.py
+++ common_prime_divisors/prime_divisors.py
@@ -1,25 +1,25 @@
 def gcd(a,b):
-    if b == 0:
-       return a 
-    return gcd(b,a % b)
+   if b == 0:
+      return a 
+   return gcd(b,a % b)
 def solution(A,B):
-    count = 0 
-    for i in range(0,len(A)):
-       a = A[i]
-       b = B[i]
-       p = gcd(a,b) 
-       while True :
-          d = gcd(a,p)
-          if d == 1:
-             break
-          a = a // d 
-       while True :
-          d = gcd(b,p)
-          if d == 1:
-             break
-          b = b // d 
-       if a == 1 and b == 1 :
+   count = 0 
+   for i in range(0,len(A)):
+      a = A[i]
+      b = B[i]
+      p = gcd(a,b) 
+      while True :
+         d = gcd(a,p)
+         if d == 1:
+            break
+         a = a // d 
+      while True :
+         d = gcd(b,p)
+         if d == 1:
+            break
+         b = b // d 
+      if a == 1 and b == 1 :
          count = count + 1
-       else :
-           0
-    return count 
+      else :
+         0
+   return count 
