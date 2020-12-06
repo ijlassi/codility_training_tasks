b13559878f07ca145918227d9ac7eac1220e6df4
inessJlassi <inessjlassi2016@gmail.com>
2020-12-06 15:05:17 +0100

    Algorithm 100% correct and 100% performant

--- missing_integer/missing_integer.py
+++ missing_integer/missing_integer.py
@@ -1,10 +1,10 @@
 def solution (A):
-    lista = set()
+    list1 = set()
     min = 1
     for i,v in enumerate(A,1) :
-        lista.add(v)
-    s = sorted(list(lista))
-    for i in s :
+        list1.add(v)
+    list2 = sorted(list(list1))
+    for i in list2 :
         if i == min:
             min += 1
     return min
